24c49c5d1b37dab1f139b38445add8f3bc99737b
0ga1 <andre.web16@gmail.com>
2024-08-20 13:24:49 +0300

    В методе main написал вывод списков паролей

--- pass.py
+++ pass.py
@@ -7,7 +7,7 @@ class New_password:
 • Выводит (записаный пароль внутри файла)
 • Проверяет (в процентном сотношениях от пороля)
     """
-    def __init__(self, len_pw = 8, num_pw = 1):
+    def __init__(self, len_pw=8, num_pw=1):
         a = "qwertyuiopasdfghjklzxcvbnm"
         b = "QWERTYUIOPASDFGHJKLZXCVBNM"
         c = "0123456789"
@@ -17,12 +17,20 @@ class New_password:
         self.num_pw = num_pw  # Количестов паролей
         self.len_pw = len_pw  # Длина паролей (или количество знаков)
 
+    def main(self):
+        """Проверка методов в классе"""
+        for newpw in self.pw_gen():
+            print(newpw)
+
     def pw_gen(self):
         """Генерация пароля"""
-        for i in range(self.number):
-            self.pwgen.append("".join(random.sample(self.data, self.length)))
+        for i in range(self.num_pw):
+            self.pwgen.append("".join(random.sample(self.data, self.len_pw)))
         
         return self.pwgen
 
 if __name__ == '__main__':
-    pass
+    new_pw = New_password(num_pw=5)
+    new_pw.main()
+else:
+    print('Ой!!! Ошибка вышло!')
