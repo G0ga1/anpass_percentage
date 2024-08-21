d1c563c151efd422a68ba7cd10e6c88b278655aa
0ga1 <andre.web16@gmail.com>
2024-08-21 19:25:47 +0300

    Создал в методе алгоритм проверки паролей

--- pass.py
+++ pass.py
@@ -8,43 +8,67 @@ class New_password:
 • Проверяет (в процентном сотношениях от каждого сгенерирового пароля)
     """
     def __init__(self, len_pw=8, num_pw=1):
-        a = "qwertyuiopasdfghjklzxcvbnm"
-        b = "QWERTYUIOPASDFGHJKLZXCVBNM"
-        c = "0123456789"
-        d = "[]{}()*'/,_-!?"
-        self.data = a + b + c + d  # Конкатенация
-        self.pwgen = []  # Список паролей
-        self.num_pw = num_pw  # Количестов паролей
-        self.len_pw = len_pw  # Длина паролей (или количество знаков)
+        __a = "qwertyuiopasdfghjklzxcvbnm"
+        __b = "QWERTYUIOPASDFGHJKLZXCVBNM"
+        __c = "0123456789"
+        __d = "[]{}()*'/,_-!?"
+        self.__data = __a + __b + __c + __d  # Конкатенация
+        self.nglp = []  # Новый сгенерированный список из паролей (New generated list of passwords)
+        self.__num_pw = num_pw  # Количестов паролей
+        self.__len_pw = len_pw  # Длина паролей (или количество знаков)
 
     def main(self):
         """Проверка методов в классе"""
-        passwds = '\n'.join(self.pw_gen())
-        self.pw_record()
-        print(passwds, end='')
+        self.pw_gen()
+        print(self.nglp)
+        self.ch_password()
+        print('\n', self.nglp)
 
     def pw_gen(self):
         """Генерация пароля"""
-        for i in range(self.num_pw):
-            self.pwgen.append("".join(random.sample(self.data, self.len_pw)))
+        for i in range(self.__num_pw):
+            self.nglp.append("".join(random.sample(self.__data, self.__len_pw)))
+        return self.nglp
         
-        return self.pwgen
     
     def pw_record(self):
         sh = os.path.join('password.txt', '*.srt')
         if sh:
             with open('password.txt', 'w') as file:
                 file.write('__NEW_PASSWORD__\n\n')
-                for p in range(len(self.pwgen)):
-                    pw_txt = self.pwgen[p] + '\n'
+                for p in range(len(self.nglp)):
+                    pw_txt = self.nglp[p] + '\n'
                     file.write(pw_txt)
                 file.close()
 
+
     def ch_password(self):
         """Проверка пароля на надежность"""
-        pass
+        new_pwgen = self.nglp
+        self.nglp = []
+        for i in range(len(new_pwgen)):
+            count = 0  # Потчет очков на НАДЕЖНОСТЬ паролей
+            upper_case = any([1 if i in string.ascii_uppercase else 0 for i in new_pwgen[i]])  # проверка ВЕРХНЕГО РЕГИСТРА строки
+            lower_case = any([1 if i in string.ascii_lowercase else 0 for i in new_pwgen[i]])  # проверка НИЖНИГО РЕГИСТРА строк
+            special = any([1 if i in string.punctuation else 0 for i in new_pwgen[i]])
+            digits = any([1 if i in string.digits else 0 for i in new_pwgen[i]])
+            repeated = any([len(new_pwgen[i]) != len(set(new_pwgen[i]))]) #  Проверка на дубликат СИМВОЛОВ и СПЕСИАЛЬНЫХ СИМВОЛОВ
+            checking = sum([upper_case, lower_case, special, digits, repeated])  # Объединяет алгоритм проверок из списка
 
-    def percent_pw_gen():
+            # Подчет на истинуность из спиcков
+            for num in range(1, checking+1):
+                if checking > num:
+                    count += 1
+
+            # Проверка на длину
+            total = len(new_pwgen[i])
+            count += sum(1 for j in range(8, 65) if total > j)
+
+            # Добовляет новый список паролей с ОЧКАМИ
+            self.nglp.append([new_pwgen[i], count])
+
+
+    def __percent_pw_gen():
         """Проверка в процентах"""
         pass
 
