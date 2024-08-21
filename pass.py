ffc9ee44c6a19c4c36c3bc01437d86d441972004
0ga1 <andre.web16@gmail.com>
2024-08-22 01:15:34 +0300

    Создал два новых метадом записи и создал в каждом из трёх методов алгоритм записи

--- pass.py
+++ pass.py
@@ -17,13 +17,15 @@ class New_password:
         self.__num_pw = num_pw  # Количестов паролей
         self.__len_pw = len_pw  # Длина паролей (или количество знаков)
 
+
     def main(self):
         """Проверка методов в классе"""
         self.pw_gen()
+        self.points_chpw()
+        self.percent_pw_gen()
         print(self.nglp)
-        self.ch_password()
-        print('\n', self.nglp)
-        print('\n', self.percent_pw_gen())
+        self.percentage_record()
+
 
     def pw_gen(self):
         """Генерация пароля"""
@@ -37,26 +39,48 @@ class New_password:
         if sh:
             with open('password.txt', 'w') as file:
                 file.write('__NEW_PASSWORD__\n\n')
-                for p in range(len(self.nglp)):
-                    pw_txt = self.nglp[p] + '\n'
+                for i in range(len(self.nglp)):
+                    pw_txt = self.nglp[i] + '\n'
                     file.write(pw_txt)
                 file.close()
 
 
-    def ch_password(self):
+    def points_record(self):
+        sh = os.path.join('password.txt', '*.srt')
+        if sh:
+            with open('password.txt', 'w') as file:
+                file.write('__NEW_PASSWORD_POINT__\n\n')
+                for i in range(len(self.nglp)):
+                    points_txt = self.nglp[i][0] + f' {self.nglp[i][1]}\n'
+                    file.write(points_txt)
+                file.close()
+
+
+    def percentage_record(self):
+        sh = os.path.join('password.txt', '*.srt')
+        if sh:
+            with open('password.txt', 'w') as file:
+                file.write('__NEW_PASSWORD_PERCENT__\n\n')
+                for i in range(len(self.nglp)):
+                    percent_txt = self.nglp[i][0] + f' {self.nglp[i][2]}%\n'
+                    file.write(percent_txt)
+                file.close()
+
+
+    def points_chpw(self):
         """Проверка пароля на надежность"""
         new_pwgen = self.nglp
         self.nglp = []
         for i in range(len(new_pwgen)):
             count = 0  # Потчет очков на НАДЕЖНОСТЬ паролей
-            upper_case = any([1 if i in string.ascii_uppercase else 0 for i in new_pwgen[i]])  # проверка ВЕРХНЕГО РЕГИСТРА строки
-            lower_case = any([1 if i in string.ascii_lowercase else 0 for i in new_pwgen[i]])  # проверка НИЖНИГО РЕГИСТРА строк
-            special = any([1 if i in string.punctuation else 0 for i in new_pwgen[i]])
-            digits = any([1 if i in string.digits else 0 for i in new_pwgen[i]])
-            repeated = any([len(new_pwgen[i]) != len(set(new_pwgen[i]))]) #  Проверка на дубликат СИМВОЛОВ и СПЕСИАЛЬНЫХ СИМВОЛОВ
+            upper_case = any([1 if i in string.ascii_uppercase else 0 for i in new_pwgen[i]])  # проверка на наличий ВЕРХНЕГО РЕГИСТРА в строки
+            lower_case = any([1 if i in string.ascii_lowercase else 0 for i in new_pwgen[i]])  # проверка на наличий НИЖНИГО РЕГИСТРА в строк
+            special = any([1 if i in string.punctuation else 0 for i in new_pwgen[i]])  # проверка на наличий СПЕЦСИМВОЛОВ в строке
+            digits = any([1 if i in string.digits else 0 for i in new_pwgen[i]])  # проверка на наличий НАТУРАЛЬНЫХ ЧИСЕЛ в строке
+            repeated = any([len(new_pwgen[i]) != len(set(new_pwgen[i]))]) #  Проверка на дубликат СИМВОЛОВ и СПЕСИАЛЬНЫХ СИМВОЛОВ в строке
             checking = sum([upper_case, lower_case, special, digits, repeated])  # Объединяет алгоритм проверок из списка
 
-            # Подчет на истинуность из спиcков
+            # Подчет спиcка на надженость в баллах
             for num in range(1, checking+1):
                 if checking > num:
                     count += 1
@@ -65,7 +89,7 @@ class New_password:
             total = len(new_pwgen[i])
             count += sum(1 for j in range(8, 65) if total > j)
 
-            # Добовляет новый список паролей с ОЧКАМИ
+            # Добовляет новый список паролей с БАЛЛАМИ
             self.nglp.append([new_pwgen[i], count])
 
 
@@ -73,12 +97,10 @@ class New_password:
         """Проверка в процентах"""
         lpw = len(self.nglp)
         for i in range(lpw):
-            pc = int((self.nglp[i][1] / len(self.nglp[i][0])) * 100)
-            self.nglp[i].append(min(pc, 100))
-        return self.nglp
+            pc = int((self.nglp[i][1] / len(self.nglp[i][0])) * 100)  # Конверт в ПРОЦЕНТАХ
+            self.nglp[i].append(min(pc, 100))  # Добовляет новый элемент ПРОЦЕНТ в список с паролями и баллами
+
 
 if __name__ == '__main__':
     new_pw = New_password(num_pw=12, len_pw=12)
     new_pw.main()
-else:
-    print('Ой!!! Ошибка вышло!')
