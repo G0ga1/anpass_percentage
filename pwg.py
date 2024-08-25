7c52c80fd4e63b77af5665673b03a7782e39e66f
0ga1 <andre.web16@gmail.com>
2024-08-25 06:19:10 +0300

    Проработал над ИСКЛЮЧЕНИЯМИ и ИНКАПСУЛИРУЮ атрибуты

--- pwg.py
+++ pwg.py
@@ -7,73 +7,85 @@ class New_password:
 • Выводит (сгенерированный пароль(-и) внутри текстового файла)
 • Проверяет (в процентном сотношениях от каждого сгенерирового пароля)
     """
-    def __init__(self, len_pw=8, num_pw=1):
+    def __init__(self, len_pw: int=8, num_pw: int=1):
         __a = "qwertyuiopasdfghjklzxcvbnm"
         __b = "QWERTYUIOPASDFGHJKLZXCVBNM"
         __c = "0123456789"
         __d = "[]{}()*'/,_-!?"
-        self.__data = __a + __b + __c + __d  # Конкатенация
-        self.nglp = []  # Новый сгенерированный список из паролей (New generated list of passwords)
-        self.__num_pw = num_pw  # Количестов паролей
-        self.__len_pw = len_pw  # Длина паролей (или количество знаков)
+        self.__data = __a + __b + __c + __d  # Объединения всех символов, чисел и букв ВЕРШНЕГО НИЖНЕГО регистра
+        self.__nglp = []  # Новый сгенерированный список из паролей (New generated list of passwords)
+        self.__num_pw = num_pw  # Количестов паролей в списке
+        self.__len_pw = len_pw  # Количестов знаков в паролей
 
 
     def main(self):
-        """Проверка методов в классе"""
+        """Тут проверка каждого методов внутри main"""
         self.pw_gen()
         self.points_chpw()
         self.percent_pwgen()
-        print(self.nglp)
+        print(self.__nglp)
         self.percentage_record()
 
 
-    def pw_gen(self):
+    def pw_gen(self) -> list:
         """Генерация пароля"""
-        for i in range(self.__num_pw):
-            self.nglp.append("".join(random.sample(self.__data, self.__len_pw)))
-        return self.nglp
+        try:
+            if self.__len_pw <= 7 or self.__len_pw >= 65:
+                raise SystemExit('Enter a number between 8 and 64 for this parameter len_pw')
+            elif self.__num_pw < 1 or self.__num_pw >= 65:
+                raise SystemExit('Enter a number between 1 and 64 for this parameter num_pw')
+            
+            for i in range(self.__num_pw):
+                self.__nglp.append("".join(random.sample(self.__data, self.__len_pw)))
+
+            return self.__nglp
+        except TypeError:
+            raise SystemExit('Incorrectly entered. Enter only the number')
         
     
-    def pw_record(self):
+    def pw_record(self) -> None:
         """Запись в текстовый файл пароли"""
-        sh = os.path.join('password.txt', '*.srt')
-        if sh:
-            with open('password.txt', 'w') as file:
-                file.write('__NEW_PASSWORD__\n\n')
-                for i in range(len(self.nglp)):
-                    pw_txt = self.nglp[i] + '\n'
-                    file.write(pw_txt)
-                file.close()
+        if len(self.__nglp) == 0:
+            raise SystemExit('You have not generated a password')
+        
+        with open('password.txt', 'w') as file:
+            file.write('__NEW_PASSWORD__\n\n')
+            for i in range(len(self.__nglp)):
+                pw_txt = self.__nglp[i] + '\n'
+                file.write(pw_txt)
+            file.close()
 
 
-    def points_record(self):
+    def points_record(self) -> None:
         """Запись в текстовый файл пароли и баллы(в каждого пароле)"""
-        sh = os.path.join('password.txt', '*.srt')
-        if sh:
-            with open('password.txt', 'w') as file:
-                file.write('__NEW_PASSWORD_POINT__\n\n')
-                for i in range(len(self.nglp)):
-                    points_txt = self.nglp[i][0] + f' {self.nglp[i][1]}\n'
-                    file.write(points_txt)
-                file.close()
+        if len(self.__nglp[0]) != 2:
+            raise SystemExit('NOT generated POINTS')
+        
+        with open('password.txt', 'w') as file:
+            file.write('__NEW_PASSWORD_POINT__\n\n')
+            for i in range(len(self.__nglp)):
+                points_txt = self.__nglp[i][0] + f' {self.__nglp[i][1]}\n'
+                file.write(points_txt)
+            file.close()
 
 
-    def percentage_record(self):
+    def percentage_record(self) -> None:
         """Запись в текстовый файл пароли и проценты(в каждого пароле)"""
-        sh = os.path.join('password.txt', '*.srt')
-        if sh:
-            with open('password.txt', 'w') as file:
-                file.write('__NEW_PASSWORD_PERCENT__\n\n')
-                for i in range(len(self.nglp)):
-                    percent_txt = self.nglp[i][0] + f' {self.nglp[i][2]}%\n'
-                    file.write(percent_txt)
-                file.close()
+        if len(self.__nglp[0]) != 3:
+            raise SystemExit('NOT generated PERCENTAGE')
+        
+        with open('password.txt', 'w') as file:
+            file.write('__NEW_PASSWORD_PERCENT__\n\n')
+            for i in range(len(self.__nglp)):
+                percent_txt = self.__nglp[i][0] + f' {self.__nglp[i][2]}%\n'
+                file.write(percent_txt)
+            file.close()
 
 
-    def points_chpw(self):
+    def points_chpw(self) -> None:
         """Проверка пароля на надежность"""
-        new_pwgen = self.nglp
-        self.nglp = []
+        new_pwgen = self.__nglp
+        self.__nglp = []
         for i in range(len(new_pwgen)):
             count = 0  # Потчет очков на НАДЕЖНОСТЬ паролей
             upper_case = any([1 if i in string.ascii_uppercase else 0 for i in new_pwgen[i]])  # проверка на наличий ВЕРХНЕГО РЕГИСТРА в строки
@@ -93,17 +105,17 @@ class New_password:
             count += sum(1 for j in range(8, 65) if total > j)
 
             # Добовляет новый список паролей с БАЛЛАМИ
-            self.nglp.append([new_pwgen[i], count])
+            self.__nglp.append([new_pwgen[i], count])
 
 
-    def percent_pwgen(self):
+    def percent_pwgen(self) -> None:
         """Проверка в процентах"""
-        lpw = len(self.nglp)
+        lpw = len(self.__nglp)
         for i in range(lpw):
-            pc = int((self.nglp[i][1] / len(self.nglp[i][0])) * 100)  # Конверт в ПРОЦЕНТАХ
-            self.nglp[i].append(min(pc, 100))  # Добовляет новый элемент ПРОЦЕНТ в список с паролями и баллами
+            pc = int((self.__nglp[i][1] / len(self.__nglp[i][0])) * 100)  # Конверт в ПРОЦЕНТАХ
+            self.__nglp[i].append(min(pc, 100))  # Добовляет новый элемент ПРОЦЕНТ в список с ПАРОЛЯМИ и БАЛЛАМИ
 
 
 if __name__ == '__main__':
-    new_pw = New_password(num_pw=12, len_pw=12)
+    new_pw = New_password()
     new_pw.main()
